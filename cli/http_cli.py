20b3c9a72e34ec4a65217b18d4c15e91c6acc422
lingfliu <lingfeng.liu@163.com>
2024-05-12 16:18:04 +0800

    updated clis

--- cli/http_cli.py
+++ cli/http_cli.py
@@ -1,15 +1,38 @@
 import requests
 class HttpCli:
     """http client using post request to submit result to server"""
-    def __init__(self, url):
+    def __init__(self, url, token=None):
         self.url = url
+        self.token = token
 
-    def get(self, path):
+
+    def _get(self, path, headers=None):
+        if not self.token:
+            return None
+
         url = self.url + path
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         return response.json()
 
-    def post(self, path, data):
+    def _post(self, path, data, headers=None):
+        if not self.token:
+            return None
+
         url = self.url + path
-        response = requests.post(url, json=data)
+        response = requests.post(url, json=data, headers=headers)
         return response.json()
+
+    def submit(self, tid, result):
+        """ result submission"""
+        if not self.token:
+            return None
+
+        return self._post('/submit', {'tid': tid, 'result': result}, headers={'Authorization': self.token})
+
+    def fetch(self):
+        """ fetch result by tid"""
+        if not self.token:
+            return None
+
+        # put token in header
+        return self._get('/fetch/', headers={'Authorization': self.token})
--- cli/mq_cli.py
+++ cli/mq_cli.py
@@ -1,16 +1,31 @@
-class MqCli:
-    '''MQ class to connect, publish and subscribe to MQ
-        by default, using NSQ
-    '''
+import nsq
 
-    def __int__(self):
-        pass
+class MqCli:
+    """MQ cli, by default, using NSQ
+    """
+
+    def __init__(self):
+        self.rx = None
+
+    def __int__(self, host, port, topic, channel):
+        self.topic = topic
+        self.host = host
+        self.port = port
+        self.channel = channel
+        self.nsq = nsq
+        self.rx = None
+        self.tx = nsq.Writer(nsqd_tcp_addresses=[self.host + ':' + self.port])
 
     def connect(self):
-        print("Connected to MQ")
+        self.nsq.run()
 
     def publish(self, msg):
-        print("Published to MQ")
+        self.nsq.pub(self.topic, msg)
+
+    def message_handle(self, msg):
+        print(msg.body)
 
     def subscribe(self, topic):
-        print(f"Subscribed to {topic}")
+        self.rx = nsq.Reader(message_handler=self.message_handle,
+                             lookupd_http_addresses=[self.host + ':' + self.port],
+                             topic=topic, channel=self.channel, lookupd_poll_interval=15)
