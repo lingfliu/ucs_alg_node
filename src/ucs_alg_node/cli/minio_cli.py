55ba50e929df706f663bd19d6bbed4aab0c43f15
chenghao2412 <78187368+chenghao2412@users.noreply.github.com>
2024-05-23 11:07:37 +0800

    minio补充
    实现本地上传以及下载，对一些报错进行修改

---
+++ src/ucs_alg_node/cli/minio_cli.py
@@ -0,0 +1,71 @@
+from minio import Minio
+from minio.error import S3Error
+class MinioCli:
+    def __init__(self, host, port, bucket, username, passwd):
+        self.host = host
+        self.port = port
+        self.bucket = bucket
+        self.username = username
+        self.passwd = passwd
+
+        try:
+            self.cli = Minio(host+':'+port, access_key=username, secret_key=passwd, secure=False)
+            found = self.cli.bucket_exists(bucket)
+            if not found:
+                self.cli.make_bucket(bucket)
+                print("Created bucket", bucket)
+            else:
+                print("Bucket", bucket, "already exists")
+        except S3Error as e:
+            print(e)
+            self.cli = None
+
+    def upload(self, obj_name, obj_path):
+        if not self.cli:
+            return -1
+        try:
+            self.cli.fput_object(self.bucket, obj_name, obj_path)
+            return 0
+        except S3Error as e:
+            print(e)
+            return -1
+
+    def download(self, obj_name, obj_path):
+        if not self.cli:
+            return -1
+        try:
+            self.cli.fget_object(self.bucket, obj_name, obj_path)
+            return 0
+        except S3Error as e:
+            print(e)
+            return -1
+
+    def query(self, obj_name):
+        if not self.cli:
+            return -1
+        try:
+            return self.cli.stat_object(self.bucket, obj_name)
+        except S3Error as e:
+            print(e)
+            return -1
+
+    def query_all(self):
+        if not self.cli:
+            return -1
+        try:
+            return self.cli.list_objects(self.bucket)
+        except S3Error as e:
+            print(e)
+            return -1
+
+    def count(self):
+        if not self.cli:
+            return -1
+        try:
+            return len(self.cli.list_objects(self.bucket))
+        except S3Error as e:
+            print(e)
+            return -1
+
+
+
---
+++ test/test_cli.py
@@ -0,0 +1,83 @@
+from src.ucs_alg_node.cli import *
+
+def test_mqtt_cli():
+    mqtt_cli = MqttCli({
+
+    })
+
+    def on_connect():
+        print('connected to EMQX')
+    mqtt_cli.on_connect = on_connect
+
+
+    mqtt_cli.subscribe('ucs/alg/res')
+    mqtt_cli.publish('ucs/alg/res', {
+        'status': 'ok',
+        'msg': 'connected'
+    })
+
+def test_redis_cli():
+    redis_cli = RedisCli({
+        'host':'localhost',
+        'port':6379,
+        'password':'123456'
+    })
+    redis_cli.connect()
+
+    redis_cli.set('test', 'test')
+    redis_cli.get('test')
+
+
+def test_mq_cli():
+    def on_message(msg):
+        print('received: ', msg)
+
+    mq_cli = MqCli({
+        'host': 'localhost:9092',
+        'topic': 'test',
+        'channel': 'test',
+        'on_message': on_message
+        })
+
+    mq_cli.connect()
+    mq_cli.publish({
+        'status': 'ok',
+        'err': None
+    })
+
+
+def test_minio_cli():
+    """test for minio upload, download, query, count"""
+    minio_cli = MinioCli(
+        'localhost',
+         '9090',
+        'pubilc',#这里好像只能起bucket名字，不能用文件夹
+         'admin',
+        'admin1234'
+    )
+
+    fobj_list = minio_cli.query_all()
+    # print(fobj_list)
+    for fobj in fobj_list:
+        # todo: download all fobj_list
+        # minio_cli.download()
+        pass
+
+    # The file to upload, change this path if needed 本地文件上传
+    # source_file = r"D:\py program\ucs_alg_node\requirements.txt"
+    #
+    # destination_file = "requirements05.txt"
+    # minio_cli.upload(destination_file, source_file)
+    # The file to download, change this path if needed 文件下载
+    source_file = r"D:\py program\ucs_alg_node\requirements01.txt"
+    destination_file = "requirements05.txt"
+    minio_cli.download(destination_file,source_file)
+
+
+
+if __name__ == '__main__':
+    # test_mqtt_cli()
+    # test_redis_cli()
+    # test_mqtt_cli()
+
+    test_minio_cli()
