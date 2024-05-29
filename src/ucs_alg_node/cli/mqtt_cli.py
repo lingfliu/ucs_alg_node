49f36cf0b308835defc3a3808cc89983755b998e
chenghao2412 <78187368+chenghao2412@users.noreply.github.com>
2024-05-29 16:54:25 +0800

    mqtt代码修改
    实现正常接受和发布

--- src/ucs_alg_node/cli/mqtt_cli.py
+++ src/ucs_alg_node/cli/mqtt_cli.py
@@ -1,8 +1,6 @@
 import threading
 import time
-
 import paho.mqtt.client as mqtt_client
-
 import random
 
 STAT_DISCONNECTED = 'disconnected'
@@ -10,11 +8,7 @@ STAT_CONNECTING = 'disconnected'
 STAT_CONNECTED = 'connected'
 STAT_SUBSCRIBED = 'subscribed'
 
-
 class MqttCli:
-    """N.B.: any MQ may bet lost, if the algorithm is in batch mode, should pass the result to
-    a database or a file
-    """
     def __init__(self, host, port, username, passwd, topics, cb_on_message=None):
         self.host = host
         self.port = port
@@ -27,40 +21,23 @@ class MqttCli:
         self.client.username_pw_set(self.username, self.passwd)
 
         self.topics = topics
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
 
-        # register callbacks
-        def on_connect(client, userdata, flags, rc):
-            if rc == 0:
-                print("Connected to MQTT Broker!")
-                self.stat = STAT_CONNECTED
-            else:
-                print("Failed to connect, return code %d\n", rc)
-                self.stat = STAT_DISCONNECTED
-            for topic in topics:
-                self.client.subscribe(topic)
-        self.client.on_connect = on_connect
-
-        def on_disconnect(client, userdata, rc):
-            print("Disconnected from MQTT Broker!")
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+            self.stat = STAT_CONNECTED
+            # 订阅主题列表
+            self.subscribe()
+        else:
+            print(f"Failed to connect, return code {rc}")
             self.stat = STAT_DISCONNECTED
-            time.sleep(0.01)
-            # auto-reconnect
-            self.connect()
 
-        self.client.on_disconnect = on_disconnect
-
-        def on_publish():
-            pass
-        self.client.on_publish = on_publish
-
-        self.cb_on_message = cb_on_message
-
-        def on_message(client, userdata, message):
-            # print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
-            if self.cb_on_message:
-                self.cb_on_message(message.topic, message.payload.decode())
-
-        self.client.on_message = on_message
+    def on_message(self, client, userdata, message):
+        print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
+        # if self.cb_on_message:
+        #     self.cb_on_message(message.topic, message.payload.decode())
 
     def connect(self):
         self.stat = STAT_CONNECTING
@@ -68,7 +45,7 @@ class MqttCli:
         self.client.connect(host=self.host, port=self.port, keepalive=0)
         # todo: check the loop function to see if it is blocking
         self.client.loop_start()
-        self.client.loop()
+        # self.client.loop()
         return 0
 
     def disconnect(self):
@@ -76,13 +53,22 @@ class MqttCli:
         self.client.disconnect()
         self.client.loop_stop(force=True)
 
-    def subscribe(self, topic):
-        self.client.subscribe(topic)
+    def subscribe(self):
+        # 订阅单个主题或者多个主题
+        if isinstance(self.topics, str):
+            self.client.subscribe(self.topics)
+        elif isinstance(self.topics, list):
+            for topic in self.topics:
+                self.client.subscribe(topic)
+        else:
+            print("Invalid topics format. Should be a string or a list of strings.")
 
     def publish(self, message):
         """publish to default topics[0]"""
-        self.client.publish(self.topics[0], message)
+        self.client.publish(self.topics, message)
+        print(f"Send `{message}` to topic `{self.topics}`")
 
     def publish_to(self, topic, message):
         """specify topic to publish"""
         self.client.publish(topic, message)
+        print(f"Send `{message}` to topic `{topic}`")
--- test/test_cli.py
+++ test/test_cli.py
@@ -1,23 +1,42 @@
 from src.ucs_alg_node.cli import *
-
+import json
+import time
 def test_mqtt_cli():
     mqtt_cli = MqttCli(
-        host = 'localhost',
-        port = '18083',
-        username = 'admin',#默认账号
-        passwd = 'public',#默认密码
-        topics = None
+        host='localhost',
+        port=1883,
+        username='admin',  # 默认账号
+        passwd='public',  # 默认密码
+        topics='/ucs/alg'
     )
 
     def on_connect():
         print('connected to EMQX')
-    mqtt_cli.on_connect = on_connect()
 
-    mqtt_cli.subscribe('ucs/alg/res')
-    mqtt_cli.publish('ucs/alg/res', {
-        'status': 'ok',
-        'msg': 'connected'
-    })
+    mqtt_cli.on_connect = on_connect()
+    # 连接到 MQTT 代理
+    mqtt_cli.connect()
+    # 新建特定话题
+    topic = "ucs/alg/res"
+    # 订阅话题
+    mqtt_cli.subscribe()
+    # 监听订阅的消息发布
+    # try:
+    #     # 循环以保持连接并处理消息
+    #     while True:
+    #         # 在此处添加你的其他逻辑
+    #         time.sleep(1)  # 这里可以添加其他逻辑，同时保持程序在运行
+    # except KeyboardInterrupt:
+    #     print("Interrupted")
+
+    # 发布默认话题topics
+    mqtt_cli.publish(json.dumps({
+        "status": "ok",
+        "msg": "connected"}))
+    # # 发布特定话题
+    mqtt_cli.publish_to(topic, json.dumps({
+        "status": "ok",
+        "msg": "connected"}))
 
 def test_redis_cli():
     redis_cli = RedisCli(
@@ -26,7 +45,7 @@ def test_redis_cli():
         db=0,  # 默认数据库索引
         username=None,  # 如果没有用户名认证，则留空
         passwd='123456'
-                         )
+    )
     redis_cli.connect()
 
     redis_cli.set('test', 'test')
@@ -42,7 +61,7 @@ def test_mq_cli():
         'topic': 'test',
         'channel': 'test',
         'on_message': on_message
-        })
+    })
 
     mq_cli.connect()
     mq_cli.publish({
@@ -56,7 +75,7 @@ def test_minio_cli():
     minio_cli = MinioCli(
         host='localhost',
         port='9090',
-        bucket='pubilc',#这里好像只能起bucket名字，不能用文件夹
+        bucket='pubilc',  # 这里好像只能起bucket名字，不能用文件夹
         username='admin',
         passwd='admin1234'
     )
@@ -76,13 +95,12 @@ def test_minio_cli():
     # The file to download, change this path if needed 文件下载
     source_file = r"D:\py program\ucs_alg_node\requirements05.txt"
     destination_file = "requirements01.txt"
-    minio_cli.download(destination_file,source_file)
-
+    minio_cli.download(destination_file, source_file)
 
 
 if __name__ == '__main__':
     # test_mqtt_cli()
-    test_redis_cli()
-    # test_mqtt_cli()
+    # test_redis_cli()
+    test_mqtt_cli()
 
     # test_minio_cli()
