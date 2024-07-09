c4295518b9023c7bebd77dcc6ec659030b29e873
lingfliu <lingfeng.liu@163.com>
2024-07-10 01:15:50 +0800

    TESTPASS nsq pub & subscribe using channels

--- test/nsq_test.py
+++ test/nsq_test.py
@@ -2,9 +2,14 @@
 #
 # from src.ucs_alg_node.cli import NsqCli
 #
-host = '62.234.16.239'
-# # host = 'localhost'
-port = 4171
+# host = '62.234.16.239'
+# host = '192.168.0.103'
+import time
+
+host = 'localhost'
+
+import tornado.ioloop
+
 #
 # default_topic = 'ucs/alg/result'
 # def on_msg(msg):
@@ -24,10 +29,28 @@ port = 4171
 #     time.sleep(1)
 
 import nsq
+from src.ucs_alg_node.utils import InterruptableThread
 
 def handle_msg(msg):
-    print('received: ', msg.body)
+    # print('received: %s at '%msg.body + str(time.time_ns()))
+    tic = int(msg.body.split(b':')[1])
+    toc = time.time_ns()
+    print('latency: %d ns'%(toc - tic))
     return True
-r = nsq.Reader(message_handler=handle_msg, nsqd_tcp_addresses=[host + ':4171'], topic='test_topic', channel='nsqxxd', lookupd_poll_interval=15)
+r = nsq.Reader(message_handler=handle_msg, nsqd_tcp_addresses=[host + ':4150'], topic='test', channel='ucs', lookupd_poll_interval=15)
+
+w = nsq.Writer([host + ':4150'])
+
+def _on_pub_finished(conn, data):
+    # print(data)
+    pass
+
+def _pub_task():
+    for _ in range(1):
+        w.pub('test', bytes('ts:%d'%time.time_ns(), 'utf-8'), _on_pub_finished)
+
+tornado.ioloop.PeriodicCallback(_pub_task, 1).start()
 
 nsq.run()
+while True:
+    time.sleep(1)
